181a549b479c1f240a89d58cdfc55633030d8815
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-05 20:34:39 +0500

    Create postgres.go

---
+++ backend/internal/db/postgres.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func Connect() {
+	dsn := os.Getenv("DB_URL")
+
+	var err error
+	DB, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("✅ DB connected")
+}
