9120edfb6b857b407345a809597c1357c5eb8662
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-05 21:00:51 +0500

    Update r2.go

--- backend/internal/storage/r2.go
+++ backend/internal/storage/r2.go
@@ -1,58 +1,32 @@
 package storage
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"mime/multipart"
-	"os"
+	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-var client *s3.Client
+func NewR2Client(accountID, accessKey, secretKey string) *s3.Client {
+	endpoint := "https://" + accountID + ".r2.cloudflarestorage.com"
 
-func InitR2() {
-	cfg, _ := config.LoadDefaultConfig(context.TODO(),
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithRegion("auto"),
 		config.WithCredentialsProvider(
-			aws.NewCredentialsCache(
-				aws.StaticCredentialsProvider{
-					Value: aws.Credentials{
-						AccessKeyID:     os.Getenv("R2_ACCESS_KEY"),
-						SecretAccessKey: os.Getenv("R2_SECRET_KEY"),
-					},
-				},
-			),
+			credentials.NewStaticCredentialsProvider(accessKey, secretKey, ""),
 		),
 	)
 
-	client = s3.NewFromConfig(cfg, func(o *s3.Options) {
-		o.BaseEndpoint = aws.String(os.Getenv("R2_ENDPOINT"))
-	})
-}
-
-func Upload(file multipart.File, filename string) (string, error) {
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(file)
-
-	_, err := client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(os.Getenv("R2_BUCKET")),
-		Key:    aws.String(filename),
-		Body:   bytes.NewReader(buf.Bytes()),
-	})
-
 	if err != nil {
-		return "", err
+		log.Fatal(err)
 	}
 
-	url := fmt.Sprintf("%s/%s/%s",
-		os.Getenv("R2_ENDPOINT"),
-		os.Getenv("R2_BUCKET"),
-		filename,
-	)
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(endpoint)
+	})
 
-	return url, nil
+	return client
 }
