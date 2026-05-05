bdde7df66339e56e076254a14b0f7e7a3ef25ae2
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-05 22:06:27 +0500

    Update main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,37 +5,20 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-
-	"tajikshop/internal/db"
-	"tajikshop/internal/handlers"
 )
 
 func main() {
-	// load .env (локально)
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No .env file found (ok in production)")
-	}
-
-	// connect database
-	db.Connect()
-
-	// router
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "ok"})
+		c.JSON(200, gin.H{"message": "🚀 TajikShop LIVE"})
 	})
 
-	r.POST("/products", handlers.CreateProduct)
-
-	// port
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Println("🚀 running on", port)
+	log.Println("Server running on", port)
 	r.Run(":" + port)
 }
