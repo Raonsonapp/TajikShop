493ac15e682426e6b1321d4dfdc53b7c6264547a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-06 08:34:07 +0500

    Delete backend/internal/models/product.go

--- backend/internal/models/product.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Product struct {
-	ID          string  `json:"id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-	ImageURL    string  `json:"image_url"`
-}
