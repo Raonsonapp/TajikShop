afc6ac12dbff2baedf0fd9a761a34b32eae5f3aa
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-07 15:46:27 +0500

    Create exceptions.dart

---
+++ lib/core/errors/exceptions.dart
@@ -0,0 +1,19 @@
+class ServerException implements Exception {
+  final String message;
+  final int? statusCode;
+  const ServerException({required this.message, this.statusCode});
+}
+
+class NetworkException implements Exception {
+  final String message;
+  const NetworkException({required this.message});
+}
+
+class UnauthorizedException implements Exception {
+  const UnauthorizedException();
+}
+
+class NotFoundException implements Exception {
+  final String message;
+  const NotFoundException({required this.message});
+}
