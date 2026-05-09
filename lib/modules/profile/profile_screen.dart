0a1d3a2887c4a9ccc1b61655613f37e6e007976e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-09 22:50:41 +0500

    Update profile_screen.dart

--- lib/modules/profile/profile_screen.dart
+++ lib/modules/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
