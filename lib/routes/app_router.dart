427fef7fdea297afaa48ab6e40d3704f48e2f47a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-09 05:10:35 +0500

    Update app_router.dart

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -5,7 +5,7 @@ import '../modules/splash/splash_screen.dart';
 import '../modules/auth/login_screen.dart';
 import '../modules/auth/register_screen.dart';
 import '../modules/home/home_screen.dart';
-import '../modules/search/search_screen.dart';
+import '../modules/upload/upload_screen.dart';
 import '../modules/categories/categories_screen.dart';
 import '../modules/product/product_detail_screen.dart';
 import '../modules/cart/cart_screen.dart';
@@ -22,86 +22,28 @@ import 'route_names.dart';
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: RouteNames.splash,
-    debugLogDiagnostics: false,
     errorBuilder: (_, __) => const Scaffold(
       backgroundColor: Color(0xFF0A0A0F),
-      body: Center(
-        child: Text('Саҳифа ёфт нашуд',
-            style: TextStyle(color: Colors.white)),
-      ),
-    ),
+      body: Center(child: Text('Саҳифа ёфт нашуд', style: TextStyle(color: Colors.white)))),
     routes: [
-      GoRoute(
-        path: RouteNames.splash,
-        builder: (_, __) => const SplashScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.login,
-        builder: (_, __) => const LoginScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.register,
-        builder: (_, __) => const RegisterScreen(),
-      ),
-      GoRoute(
-        path: '/product/:id',
-        builder: (_, state) =>
-            ProductDetailScreen(id: state.pathParameters['id']!),
-      ),
-      GoRoute(
-        path: RouteNames.orders,
-        builder: (_, __) => const OrdersScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.notifications,
-        builder: (_, __) => const NotificationsScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.categories,
-        builder: (_, __) => const CategoriesScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.seller,
-        builder: (_, __) => const SellerDashboardScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.addProduct,
-        builder: (_, __) => const AddProductScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.admin,
-        builder: (_, __) => const AdminDashboardScreen(),
-      ),
-      // Shell with bottom nav - LAST
+      GoRoute(path: RouteNames.splash, builder: (_, __) => const SplashScreen()),
+      GoRoute(path: RouteNames.login, builder: (_, __) => const LoginScreen()),
+      GoRoute(path: RouteNames.register, builder: (_, __) => const RegisterScreen()),
+      GoRoute(path: '/product/:id', builder: (_, s) => ProductDetailScreen(id: s.pathParameters['id']!)),
+      GoRoute(path: RouteNames.orders, builder: (_, __) => const OrdersScreen()),
+      GoRoute(path: RouteNames.notifications, builder: (_, __) => const NotificationsScreen()),
+      GoRoute(path: RouteNames.categories, builder: (_, __) => const CategoriesScreen()),
+      GoRoute(path: RouteNames.seller, builder: (_, __) => const SellerDashboardScreen()),
+      GoRoute(path: RouteNames.addProduct, builder: (_, __) => const AddProductScreen()),
+      GoRoute(path: RouteNames.admin, builder: (_, __) => const AdminDashboardScreen()),
       ShellRoute(
-        builder: (context, state, child) =>
-            MainScaffold(child: child),
+        builder: (context, state, child) => MainScaffold(child: child),
         routes: [
-          GoRoute(
-            path: RouteNames.home,
-            pageBuilder: (_, __) => const NoTransitionPage(
-                child: HomeScreen()),
-          ),
-          GoRoute(
-            path: RouteNames.search,
-            pageBuilder: (_, __) => const NoTransitionPage(
-                child: SearchScreen()),
-          ),
-          GoRoute(
-            path: RouteNames.favorites,
-            pageBuilder: (_, __) => const NoTransitionPage(
-                child: FavoritesScreen()),
-          ),
-          GoRoute(
-            path: RouteNames.cart,
-            pageBuilder: (_, __) =>
-                const NoTransitionPage(child: CartScreen()),
-          ),
-          GoRoute(
-            path: RouteNames.profile,
-            pageBuilder: (_, __) => const NoTransitionPage(
-                child: ProfileScreen()),
-          ),
+          GoRoute(path: RouteNames.home, pageBuilder: (_, __) => const NoTransitionPage(child: HomeScreen())),
+          GoRoute(path: RouteNames.favorites, pageBuilder: (_, __) => const NoTransitionPage(child: FavoritesScreen())),
+          GoRoute(path: RouteNames.upload, pageBuilder: (_, __) => const NoTransitionPage(child: UploadScreen())),
+          GoRoute(path: RouteNames.cart, pageBuilder: (_, __) => const NoTransitionPage(child: CartScreen())),
+          GoRoute(path: RouteNames.profile, pageBuilder: (_, __) => const NoTransitionPage(child: ProfileScreen())),
         ],
       ),
     ],
