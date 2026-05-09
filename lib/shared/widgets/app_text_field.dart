64a2058a328a21300503a16cbd04a235e33d4e2e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-09 05:39:20 +0500

    Update app_text_field.dart

--- lib/shared/widgets/app_text_field.dart
+++ lib/shared/widgets/app_text_field.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../../core/constants/app_colors.dart';
 
 class AppTextField extends StatelessWidget {
   final String hint;
   final IconData? prefixIcon;
   final IconData? suffixIcon;
+  final Widget? suffixWidget;
   final VoidCallback? onSuffixTap;
   final bool obscure;
   final TextEditingController? controller;
@@ -12,12 +14,14 @@ class AppTextField extends StatelessWidget {
   final String? Function(String?)? validator;
   final void Function(String)? onChanged;
   final int maxLines;
+  final List<TextInputFormatter>? inputFormatters;
 
   const AppTextField({
     super.key,
     required this.hint,
     this.prefixIcon,
     this.suffixIcon,
+    this.suffixWidget,
     this.onSuffixTap,
     this.obscure = false,
     this.controller,
@@ -25,6 +29,7 @@ class AppTextField extends StatelessWidget {
     this.validator,
     this.onChanged,
     this.maxLines = 1,
+    this.inputFormatters,
   });
 
   @override
@@ -35,7 +40,8 @@ class AppTextField extends StatelessWidget {
       keyboardType: keyboardType,
       validator: validator,
       onChanged: onChanged,
-      maxLines: maxLines,
+      maxLines: obscure ? 1 : maxLines,
+      inputFormatters: inputFormatters,
       style: const TextStyle(color: AppColors.textPrimary, fontSize: 15),
       decoration: InputDecoration(
         hintText: hint,
@@ -43,30 +49,25 @@ class AppTextField extends StatelessWidget {
         filled: true,
         fillColor: AppColors.bgSurface,
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide.none,
-        ),
+            borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
         enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: AppColors.border, width: 0.5),
-        ),
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: AppColors.border, width: 0.5)),
         focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: AppColors.primary, width: 2),
-        ),
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: AppColors.primary, width: 2)),
         errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: AppColors.error),
-        ),
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: AppColors.error)),
         prefixIcon: prefixIcon != null
             ? Icon(prefixIcon, color: AppColors.textMuted, size: 20)
             : null,
-        suffixIcon: suffixIcon != null
-            ? GestureDetector(
-                onTap: onSuffixTap,
-                child: Icon(suffixIcon, color: AppColors.textMuted, size: 20),
-              )
-            : null,
+        suffixIcon: suffixWidget ??
+            (suffixIcon != null
+                ? GestureDetector(
+                    onTap: onSuffixTap,
+                    child: Icon(suffixIcon, color: AppColors.textMuted, size: 20))
+                : null),
       ),
     );
   }
