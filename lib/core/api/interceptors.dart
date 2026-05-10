30fc58b09f7a78c96cc1d2fce8376a049c5df48b
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-10 05:10:33 +0500

    Update interceptors.dart

--- lib/core/api/interceptors.dart
+++ lib/core/api/interceptors.dart
@@ -1,40 +1,21 @@
 import 'package:dio/dio.dart';
 
-class AuthInterceptor extends Interceptor {
-  final String? token;
-  AuthInterceptor({this.token});
-  @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    if (token != null && token!.isNotEmpty) {
-      options.headers['Authorization'] = 'Bearer $token';
-    }
-    handler.next(options);
-  }
-}
-
-// Auto-retry up to 2 times on timeout (helps slow internet like H/0.1KB/s)
 class RetryInterceptor extends Interceptor {
   final Dio dio;
   RetryInterceptor(this.dio);
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) async {
-    final options = err.requestOptions;
-    final retries = (options.extra['retries'] as int?) ?? 0;
-
-    // Only retry on timeout/connection errors, max 2 retries
+    final opts = err.requestOptions;
+    final retries = (opts.extra['retries'] as int?) ?? 0;
     if (retries < 2 && (
         err.type == DioExceptionType.connectionTimeout ||
         err.type == DioExceptionType.receiveTimeout ||
         err.type == DioExceptionType.sendTimeout ||
         err.type == DioExceptionType.connectionError)) {
-      options.extra['retries'] = retries + 1;
-      // Exponential backoff: 2s, 4s
+      opts.extra['retries'] = retries + 1;
       await Future.delayed(Duration(seconds: 2 * (retries + 1)));
-      try {
-        final response = await dio.fetch(options);
-        return handler.resolve(response);
-      } catch (_) {}
+      try { return handler.resolve(await dio.fetch(opts)); } catch (_) {}
     }
     handler.next(err);
   }
@@ -43,30 +24,29 @@ class RetryInterceptor extends Interceptor {
 class ErrorInterceptor extends Interceptor {
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
-    String message;
+    String msg;
     switch (err.response?.statusCode) {
-      case 400: message = err.response?.data?['message'] ?? 'Дархости нодуруст'; break;
-      case 401: message = 'Иҷозат нест. Лутфан дубора ворид шавед'; break;
-      case 403: message = 'Дастрасӣ манъ аст'; break;
-      case 404: message = 'Ёфт нашуд'; break;
-      case 422: message = err.response?.data?['message'] ?? 'Маълумоти нодуруст'; break;
-      case 429: message = 'Хеле зиёд дархост. Каме сабр кунед'; break;
-      case 500: message = 'Хатои сервер. Баъдтар кӯшиш кунед'; break;
+      case 400: msg = err.response?.data?['message'] ?? 'Дархости нодуруст'; break;
+      case 401: msg = 'Иҷозат нест. Аз нав ворид шавед'; break;
+      case 403: msg = 'Дастрасӣ манъ аст'; break;
+      case 404: msg = 'Ёфт нашуд'; break;
+      case 422: msg = err.response?.data?['message'] ?? 'Маълумоти нодуруст'; break;
+      case 500: msg = 'Хатои сервер. Баъдтар кӯшиш кунед'; break;
       default:
         if (err.type == DioExceptionType.connectionTimeout ||
             err.type == DioExceptionType.receiveTimeout ||
             err.type == DioExceptionType.sendTimeout) {
-          message = 'Интернет суст аст. Дубора кӯшиш кунед';
+          msg = 'Интернет суст аст. Дубора кӯшиш кунед';
         } else if (err.type == DioExceptionType.connectionError) {
-          message = 'Пайвастшавӣ нест. Интернетро санҷед';
+          msg = 'Пайвастшавӣ нест. Интернетро санҷед';
         } else {
-          message = err.response?.data?['message'] ??
+          msg = err.response?.data?['message'] ??
               err.response?.data?['error'] ?? 'Хатои номаълум';
         }
     }
     handler.next(DioException(
       requestOptions: err.requestOptions, response: err.response,
-      type: err.type, error: message, message: message));
+      type: err.type, error: msg, message: msg));
   }
 }
 
@@ -76,12 +56,6 @@ class LoggingInterceptor extends Interceptor {
     // ignore: avoid_print
     print('→ ${o.method} ${o.path}');
     h.next(o);
-  }
-  @override
-  void onResponse(Response r, ResponseInterceptorHandler h) {
-    // ignore: avoid_print
-    print('← ${r.statusCode} ${r.requestOptions.path}');
-    h.next(r);
   }
   @override
   void onError(DioException e, ErrorInterceptorHandler h) {
