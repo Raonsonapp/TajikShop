eb182ff41bde9b318820ae67cd3002b595d6c8d5
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-10 10:44:08 +0500

    Update interceptors.dart

--- lib/core/api/interceptors.dart
+++ lib/core/api/interceptors.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 
+/// Auto-retry up to 2 times on timeout (helps H/0.1KB/s slow internet)
 class RetryInterceptor extends Interceptor {
   final Dio dio;
   RetryInterceptor(this.dio);
@@ -8,14 +9,16 @@ class RetryInterceptor extends Interceptor {
   void onError(DioException err, ErrorInterceptorHandler handler) async {
     final opts = err.requestOptions;
     final retries = (opts.extra['retries'] as int?) ?? 0;
-    if (retries < 2 && (
-        err.type == DioExceptionType.connectionTimeout ||
-        err.type == DioExceptionType.receiveTimeout ||
-        err.type == DioExceptionType.sendTimeout ||
-        err.type == DioExceptionType.connectionError)) {
+    if (retries < 2 &&
+        (err.type == DioExceptionType.connectionTimeout ||
+            err.type == DioExceptionType.receiveTimeout ||
+            err.type == DioExceptionType.sendTimeout ||
+            err.type == DioExceptionType.connectionError)) {
       opts.extra['retries'] = retries + 1;
       await Future.delayed(Duration(seconds: 2 * (retries + 1)));
-      try { return handler.resolve(await dio.fetch(opts)); } catch (_) {}
+      try {
+        return handler.resolve(await dio.fetch(opts));
+      } catch (_) {}
     }
     handler.next(err);
   }
@@ -26,12 +29,27 @@ class ErrorInterceptor extends Interceptor {
   void onError(DioException err, ErrorInterceptorHandler handler) {
     String msg;
     switch (err.response?.statusCode) {
-      case 400: msg = err.response?.data?['message'] ?? 'Дархости нодуруст'; break;
-      case 401: msg = 'Иҷозат нест. Аз нав ворид шавед'; break;
-      case 403: msg = 'Дастрасӣ манъ аст'; break;
-      case 404: msg = 'Ёфт нашуд'; break;
-      case 422: msg = err.response?.data?['message'] ?? 'Маълумоти нодуруст'; break;
-      case 500: msg = 'Хатои сервер. Баъдтар кӯшиш кунед'; break;
+      case 400:
+        msg = err.response?.data?['message'] ?? 'Дархости нодуруст';
+        break;
+      case 401:
+        msg = 'Иҷозат нест. Аз нав ворид шавед';
+        break;
+      case 403:
+        msg = 'Дастрасӣ манъ аст';
+        break;
+      case 404:
+        msg = 'Ёфт нашуд';
+        break;
+      case 422:
+        msg = err.response?.data?['message'] ?? 'Маълумоти нодуруст';
+        break;
+      case 429:
+        msg = 'Хеле зиёд дархост. Каме сабр кунед';
+        break;
+      case 500:
+        msg = 'Хатои сервер. Баъдтар кӯшиш кунед';
+        break;
       default:
         if (err.type == DioExceptionType.connectionTimeout ||
             err.type == DioExceptionType.receiveTimeout ||
@@ -41,12 +59,17 @@ class ErrorInterceptor extends Interceptor {
           msg = 'Пайвастшавӣ нест. Интернетро санҷед';
         } else {
           msg = err.response?.data?['message'] ??
-              err.response?.data?['error'] ?? 'Хатои номаълум';
+              err.response?.data?['error'] ??
+              err.message ??
+              'Хатои номаълум';
         }
     }
     handler.next(DioException(
-      requestOptions: err.requestOptions, response: err.response,
-      type: err.type, error: msg, message: msg));
+        requestOptions: err.requestOptions,
+        response: err.response,
+        type: err.type,
+        error: msg,
+        message: msg));
   }
 }
 
@@ -54,9 +77,10 @@ class LoggingInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions o, RequestInterceptorHandler h) {
     // ignore: avoid_print
-    print('→ ${o.method} ${o.path}');
+    print('→ ${o.method} ${o.path}  token:${o.headers['Authorization'] != null ? "✓" : "✗"}');
     h.next(o);
   }
+
   @override
   void onError(DioException e, ErrorInterceptorHandler h) {
     // ignore: avoid_print
