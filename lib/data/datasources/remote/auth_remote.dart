291a18d0b67cd3fb2b7e278feb22f70ce936e09f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-11 01:16:13 +0500

    Update auth_remote.dart

--- lib/data/datasources/remote/auth_remote.dart
+++ lib/data/datasources/remote/auth_remote.dart
@@ -6,12 +6,20 @@ import '../../models/user_model.dart';
 class AuthRemote {
   Dio get _dio => ApiClient.instance.dio;
 
+  // Unwrap server envelope: {"success": true, "data": {...}}
+  Map<String, dynamic> _unwrap(dynamic responseData) {
+    if (responseData is Map<String, dynamic>) {
+      return responseData['data'] as Map<String, dynamic>? ?? responseData;
+    }
+    return responseData as Map<String, dynamic>;
+  }
+
   Future<Map<String, dynamic>> login(String email, String password) async {
     final res = await _dio.post(ApiEndpoints.login, data: {
       'email':    email,
       'password': password,
     });
-    return res.data as Map<String, dynamic>;
+    return _unwrap(res.data);
   }
 
   Future<Map<String, dynamic>> register(
@@ -21,15 +29,13 @@ class AuthRemote {
       'email':    email,
       'password': password,
     });
-    return res.data as Map<String, dynamic>;
+    return _unwrap(res.data);
   }
 
   Future<UserModel> getMe() async {
     final res = await _dio.get(ApiEndpoints.me);
-    final data = res.data;
-    final map = data is Map<String, dynamic>
-        ? (data['user'] as Map<String, dynamic>? ?? data)
-        : data as Map<String, dynamic>;
+    final data = _unwrap(res.data);
+    final map = data['user'] as Map<String, dynamic>? ?? data;
     return UserModel.fromJson(map);
   }
 }
