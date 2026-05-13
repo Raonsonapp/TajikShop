c6a19bf904d56b8ecd2eb7a2a7f1855cef8486d4
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-13 14:25:43 +0500

    Update route_names.dart

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -2,6 +2,8 @@ class RouteNames {
   static const splash = '/';
   static const login = '/login';
   static const register = '/register';
+  static const phoneAuth = '/phone-auth';
+  static const phoneOtp = '/phone-otp';
   static const home = '/home';
   static const upload = '/upload';
   static const favorites = '/favorites';
@@ -14,7 +16,7 @@ class RouteNames {
   static const editProfile = '/profile/edit';
   static const addresses = '/addresses';
   static const seller = '/seller';
-  static const sellerDashboard = '/seller'; // ← ИЛОВА ШУД
+  static const sellerDashboard = '/seller/dashboard';
   static const addProduct = '/seller/add-product';
   static const admin = '/admin';
 
