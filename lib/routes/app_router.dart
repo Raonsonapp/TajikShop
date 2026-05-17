5db8bf7a16b2ffb916e3a0fa7a56b515f3a99c60
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-18 03:46:28 +0500

    Update app_router.dart

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,84 +1,45 @@
-import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../modules/splash/splash_screen.dart';
+import '../modules/auth/login_screen.dart';
+import '../modules/auth/register_screen.dart';
+import '../modules/home/home_screen.dart';
+import '../modules/profile/profile_screen.dart';
+import '../shared/widgets/main_scaffold.dart';
+import 'route_names.dart';
 
-class RetryInterceptor extends Interceptor {
-  final Dio dio;
-  RetryInterceptor(this.dio);
+final _goRouter = GoRouter(
+  initialLocation: RouteNames.splash,
+  errorBuilder: (_, __) => const Scaffold(
+    backgroundColor: Color(0xFF0A0A0F),
+    body: Center(
+      child: Text('Саҳифа ёфт нашуд',
+          style: TextStyle(color: Colors.white)))),
+  routes: [
+    GoRoute(
+      path: RouteNames.splash,
+      builder: (_, __) => const SplashScreen()),
+    GoRoute(
+      path: RouteNames.login,
+      builder: (_, __) => const LoginScreen()),
+    GoRoute(
+      path: RouteNames.register,
+      builder: (_, __) => const RegisterScreen()),
+    ShellRoute(
+      builder: (context, state, child) => MainScaffold(child: child),
+      routes: [
+        GoRoute(
+          path: RouteNames.home,
+          pageBuilder: (_, __) =>
+              const NoTransitionPage(child: HomeScreen())),
+        GoRoute(
+          path: RouteNames.profile,
+          pageBuilder: (_, __) =>
+              const NoTransitionPage(child: ProfileScreen())),
+      ],
+    ),
+  ],
+);
 
-  @override
-  Future<void> onError(DioException err, ErrorInterceptorHandler handler) async {
-    final opts = err.requestOptions;
-    final retries = (opts.extra['retries'] as int?) ?? 0;
-    if (retries < 1 &&
-        (err.type == DioExceptionType.connectionTimeout ||
-            err.type == DioExceptionType.receiveTimeout ||
-            err.type == DioExceptionType.sendTimeout ||
-            err.type == DioExceptionType.connectionError)) {
-      opts.extra['retries'] = retries + 1;
-      await Future.delayed(const Duration(milliseconds: 500));
-      try {
-        handler.resolve(await dio.fetch(opts));
-        return;
-      } catch (_) {}
-    }
-    handler.next(err);
-  }
-}
-
-class ErrorInterceptor extends Interceptor {
-  String _extractMsg(dynamic data, String fallback) {
-    if (data is Map) {
-      return data['error']?.toString() ??
-          data['message']?.toString() ??
-          fallback;
-    }
-    return fallback;
-  }
-
-  @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
-    String msg;
-    final d = err.response?.data;
-    switch (err.response?.statusCode) {
-      case 400: msg = _extractMsg(d, 'Дархости нодуруст'); break;
-      case 401: msg = _extractMsg(d, 'Email ё парол нодуруст'); break;
-      case 403: msg = 'Дастрасӣ манъ аст'; break;
-      case 404: msg = 'Ёфт нашуд'; break;
-      case 409: msg = 'Ин email аллакай вуҷуд дорад'; break;
-      case 422: msg = _extractMsg(d, 'Маълумоти нодуруст'); break;
-      case 429: msg = 'Хеле зиёд дархост. Каме сабр кунед'; break;
-      case 500: msg = 'Хатои сервер. Баъдтар кӯшиш кунед'; break;
-      default:
-        if (err.type == DioExceptionType.connectionTimeout ||
-            err.type == DioExceptionType.receiveTimeout ||
-            err.type == DioExceptionType.sendTimeout) {
-          msg = 'Интернет суст аст. Дубора кӯшиш кунед';
-        } else if (err.type == DioExceptionType.connectionError) {
-          msg = 'Пайвастшавӣ нест. Интернетро санҷед';
-        } else {
-          msg = _extractMsg(d, err.message ?? 'Хатои номаълум');
-        }
-    }
-    handler.next(DioException(
-        requestOptions: err.requestOptions,
-        response: err.response,
-        type: err.type,
-        error: msg,
-        message: msg));
-  }
-}
-
-class LoggingInterceptor extends Interceptor {
-  @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    // ignore: avoid_print
-    print('→ ${options.method} ${options.path}  auth:${options.headers['Authorization'] != null ? "✓" : "✗"}');
-    handler.next(options);
-  }
-
-  @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
-    // ignore: avoid_print
-    print('✗ ${err.response?.statusCode} ${err.requestOptions.path}: ${err.message}');
-    handler.next(err);
-  }
-}
+final routerProvider = Provider<GoRouter>((ref) => _goRouter);
